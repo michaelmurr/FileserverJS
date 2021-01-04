799090d9c2739bae8fa7d7957ea17d8a9b6628ef
Michael Murr <michael.murr04@gmail.com>
2021-01-04 22:27:00 +0100

    adapted UI to the changes made to uploads.js

--- routes/index.js
+++ routes/index.js
@@ -9,39 +9,4 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-
-/*
-router.post('/upload', function(req, res, next){
-
-  /*
-  var form = new formidable.IncomingForm();
-  form.parse(req, function(err, fields, files){
-    console.log(req);
-    var oldpath = files.filetoupload.path;
-    var newpath = '/home/mike/Documents/Coding/NodeJS/PersonalFileserver/uploadedFiles';
-    fs.rename(oldpath, newpath, function(err){
-      if(err) throw err;
-      console.log('File uploaded and moved!');
-    });
-    res.redirect('../');
-    res.end();
-  });
-//////////////////
-  if(!req.files || Object.keys(req.files).length === 0){
-    //res.redirect('../');
-    return res.status(400).send('No files were uploaded.')
-  }
-
-  let filetoupload = req.files.filetoupload;
-
-  filetoupload.mv("/home/mike/Desktop/" + files.filetoupload.Path, function(err){
-    if(err)
-      return res.status(500).send(JSON.stringify(err));
-
-    res.send('File uploaded!');
-    res.redirect('..');
-  });
-});
-*/
-
 module.exports = router;
