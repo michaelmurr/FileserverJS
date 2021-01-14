39081eb9dd2729f2843a384b817df17616d6098e
Michael Murr <michael.murr04@gmail.com>
2021-01-14 20:47:42 +0100

    removed gif allowance

--- routes/uploads.js
+++ routes/uploads.js
@@ -24,7 +24,7 @@ const upload = multer({
 // Check File Type
 function checkFileType(file, cb){
   // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif/;
+  const filetypes = /jpeg|jpg|png/;
   // Check ext
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   // Check mime
