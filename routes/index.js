dff6220777028c7c2b4de59fe96d3f1471f6d66d
Michael Murr <michael.murr04@gmail.com>
2021-01-24 15:51:10 +0100

    displays files on page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let path = require('path');
+let fs = require('fs');
 
 router.get('/', function(req, res, next) {
-  res.render('index', {msg: "E"});
+  //const directoryPath = path.dirname('/mike/home/Documents/Coding/PersonalFileserver/uploads');
+  let filesInFolder = new Array();
+  
+  //passsing directoryPath and callback function
+  fs.readdir("uploads", function (err, files) {
+    let i = 0;
+    //handling error
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    } 
+
+    console.log(files);
+    res.render('index', {msg: "E", files: files});
+  });
+  
 });
 
 module.exports = router;
