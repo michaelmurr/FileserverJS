52ef62d910c4f9e51bcd78b928ccc02e94b095d2
Michael Murr <michael.murr04@gmail.com>
2021-05-15 16:06:58 +0200

    checks if uploadsDir exists

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,25 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
-const cheerio = require('cheerio');
-const $ = cheerio.load('<h2 class="title">Hello World</h2>');
+
+let dir = "./uploads";
 
 router.get('/', function(req, res, next) {
   //const directoryPath = path.dirname('/mike/home/Documents/Coding/PersonalFileserver/uploads');
-  let filesInFolder = new Array();
   
   //passsing directoryPath and callback function
   fs.readdir("uploads", function (err, files) {
-    let i = 0;
+
+    if(!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+      res.render('index');
+    }
     //handling error
     if (err) {
       return console.log('Unable to scan directory: ' + err);
     } 
 
-    console.log(files);
+    //console.log(files);
     res.render('index', {msg: "E", files: files});
   });
 });
