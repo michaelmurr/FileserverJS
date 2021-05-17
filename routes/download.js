82b1d3ed2c4fc706e9a569f3b9ce01abf040e1af
Michael Murr <michael.murr04@gmail.com>
2021-05-17 23:03:06 +0200

    added downloading selected files from fronted

--- routes/download.js
+++ routes/download.js
@@ -1,7 +1,22 @@
 var express = require('express');
 var router = express.Router();
-router.get('/download', (req, res, next) => {
-    res.download("./uploads/test.txt");
+const zip = require("express-zip");
+
+router.post('/download', function(req, res, next) {
+    let keys = Object.keys(req.body);
+    let downloadFileArray = keys.map((file,index) => {
+        return {
+            path: "./uploads/" + file,
+            name: file
+        }
+    });
+    //console.log(JSON.stringify(downloadFileArray, undefined, 4));
+    console.log(downloadFileArray[0]);
+    if(downloadFileArray[0] != undefined ){
+        res.zip(downloadFileArray);
+
+    }
+
 });
 
 module.exports = router;
