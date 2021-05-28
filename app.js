549c0f6496522bd34cfa3c8a52a04750c9fe49af
Michael Murr <michael.murr04@gmail.com>
2021-05-28 22:43:12 +0200

    removed commented code

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-//app.use("public", express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
