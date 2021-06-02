4918b969c5ff877d5c2cee3e5f672f31ac5995f6
Michael Murr <michael.murr04@gmail.com>
2021-06-02 23:09:51 +0000

    minor fixes

--- routes/download.js
+++ routes/download.js
@@ -2,25 +2,32 @@ var express = require('express');
 var router = express.Router();
 const zip = require("express-zip");
 const getUploadsPath = require("../getUploadsPath");
+const scanDir = require('../scanDir');
 
 const dir = getUploadsPath();
 
-router.post('/download', function(req, res, next) {
+let fileData = scanDir(dir);
+
+router.post('/download', function(req, res) {
+    console.log("Download request recieved!");
     let keys = Object.keys(req.body);
-    console.log(dir + "/");
-    let downloadFileArray = keys.map((file,index) => {
-        return {
-            path: dir + "/" + file,
-            name: file
+    if(keys.length > 0){
+
+        let downloadFileArray = keys.map((file) => {
+            return {
+                path: dir + "/" + file,
+                name: file
+            }
+        });
+
+        if(downloadFileArray[0] != undefined ){
+            res.zip(downloadFileArray);
+            res.render("index", {msg: "Download starting...", fileData})
         }
-    });
-    //console.log(JSON.stringify(downloadFileArray, undefined, 4));
-    console.log(downloadFileArray[0]);
-    if(downloadFileArray[0] != undefined ){
-        res.zip(downloadFileArray);
 
     }else{
-        res.redirect("/");
+        console.log("Download: No files selected!");
+        res.render("index", {msg: "No files selected!", fileData});
     }
 });
 
