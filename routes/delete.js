18ed898f778ad4be838e50b6221290fb30289100
Michael Murr <michael.murr04@gmail.com>
2021-06-10 16:28:45 +0000

    made deleting synchronous

--- routes/delete.js
+++ routes/delete.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-const convertDataUnit = require("../convertDataUnit");
 const getUploadsPath = require("../getUploadsPath");
+const scanDir = require("../scanDir");
 
 const dir = getUploadsPath();
 
@@ -12,7 +12,7 @@ router.post("/delete", (req, res) => {
     console.log("Elements that will be removed: \n" + keys);
     //remove selected files
     keys.forEach(key => {
-        fs.rm(dir + "/" + key);
+        fs.rmSync(dir + "/" + key);
         console.log("Removed: " + key);
     });
     let fileData = scanDir(dir);
