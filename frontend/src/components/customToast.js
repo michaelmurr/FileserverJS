495d89d61c8da246eafcac2b12b0932af6afdcf5
Michael Murr <michael.murr04@gmail.com>
2022-06-13 14:57:43 +0200

    custom configured toasts

---
+++ frontend/src/components/customToast.js
@@ -0,0 +1,25 @@
+import { toast } from "react-toastify";
+
+export const successToast = (msg) => {
+  toast.success(msg, {
+    position: "bottom-right",
+    autoClose: false,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+export const errorToast = (msg) => {
+  toast.error(msg, {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
