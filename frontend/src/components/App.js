075611f0d6e1828eb83d63fd4eab0f820b09cb47
Michael Murr <michael.murr04@gmail.com>
2022-06-15 16:53:25 +0200

    removed unnecessary code, added diskstats

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -9,30 +9,10 @@ import Upload from "./Upload";
 import Files from "./Files";
 import Loginform from "./Loginform";
 import Registerform from "./Registerform";
+import Drivespace from "./Drivespace";
 
 export default function App() {
-  const [files, setFiles] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
   const [shouldUpdate, setShouldUpdate] = useState(true);
-
-  const navigate = useNavigate();
-
-  const getFiles = async () => {
-    setFiles([]);
-    const res = await fetch("/api/files");
-    if (res.status !== 200) return navigate("/login");
-    const json = await res.json();
-
-    let stuff = JSON.stringify(json);
-    let stuff2 = JSON.parse(stuff);
-    setFiles(stuff2);
-    setIsLoading(false);
-    setShouldUpdate(false);
-  };
-
-  useEffect(() => {
-    getFiles();
-  }, [shouldUpdate]);
 
   return (
     <div className="dark-bg">
@@ -41,17 +21,15 @@ export default function App() {
           path="/"
           element={
             <>
+              <Drivespace />
               <Upload
                 successToast={successToast}
                 errorToast={errorToast}
                 setShouldUpdate={setShouldUpdate}
-                isLoading={isLoading}
               />
               <Files
                 successToast={successToast}
                 errorToast={errorToast}
-                files={files}
-                isLoading={isLoading}
                 setShouldUpdate={setShouldUpdate}
                 shouldUpdate={shouldUpdate}
               />
