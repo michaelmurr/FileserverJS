d0f4c72d844942184bcdfb0e39d3150a7717060b
Michael Murr <michael.murr04@gmail.com>
2022-06-15 02:01:39 +0200

    added routing

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,20 +2,25 @@ import React, { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../css/app.css";
-
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { successToast, errorToast } from "./customToast";
 
 import Upload from "./Upload";
 import Files from "./Files";
+import Loginform from "./Loginform";
+import Registerform from "./Registerform";
 
 export default function App() {
   const [files, setFiles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [shouldUpdate, setShouldUpdate] = useState(true);
 
+  const navigate = useNavigate();
+
   const getFiles = async () => {
     setFiles([]);
     const res = await fetch("/api/files");
+    if (res.status !== 200) return navigate("/login");
     const json = await res.json();
 
     let stuff = JSON.stringify(json);
@@ -31,20 +36,31 @@ export default function App() {
 
   return (
     <div className="dark-bg">
-      <Upload
-        successToast={successToast}
-        errorToast={errorToast}
-        setShouldUpdate={setShouldUpdate}
-        isLoading={isLoading}
-      />
-      <Files
-        successToast={successToast}
-        errorToast={errorToast}
-        files={files}
-        isLoading={isLoading}
-        setShouldUpdate={setShouldUpdate}
-        shouldUpdate={shouldUpdate}
-      />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Upload
+                successToast={successToast}
+                errorToast={errorToast}
+                setShouldUpdate={setShouldUpdate}
+                isLoading={isLoading}
+              />
+              <Files
+                successToast={successToast}
+                errorToast={errorToast}
+                files={files}
+                isLoading={isLoading}
+                setShouldUpdate={setShouldUpdate}
+                shouldUpdate={shouldUpdate}
+              />
+            </>
+          }
+        />
+        <Route path="/login" element={<Loginform />} />
+        <Route path="/register" element={<Registerform />} />
+      </Routes>
       <ToastContainer
         position="bottom-right"
         autoClose={5000}
