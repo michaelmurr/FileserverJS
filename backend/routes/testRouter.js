68e071be6fc14c59106037609d89b7dd8414fda2
Michael Murr <michael.murr04@gmail.com>
2022-06-15 16:52:59 +0200

    no longer needed

--- backend/routes/testRouter.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-export default router.get("/", (req, res) => {
-  res.json({ msg: "howdy" });
-});
--- backend/routes/updateDB.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/updateDB", (req, res) => {});
-
-export default router;
